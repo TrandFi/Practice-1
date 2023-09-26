295084141ee51f335a0a71bd9556accac8d33a3a
Кирилл Каюков <kirillsport13112004@gmail.com>
2023-09-26 22:02:09 +1000

    Шаг 6. Дополнить программу доп действиями и зафиксировать изменения

--- Практика 1/Program.cs
+++ Практика 1/Program.cs
@@ -10,12 +10,15 @@ namespace Практика_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Программа, которая складывает два числа");
+            Console.WriteLine("Программа, которая складывает, вычитает, умножает и делит два числа");
             Console.Write("Введите первое число = ");
             int A = int.Parse(Console.ReadLine());
             Console.Write("Введите второе число = ");
             int B = int.Parse(Console.ReadLine());
             Console.WriteLine($"Сумма чисел равна = {A + B}");
+            Console.WriteLine($"Разность чисел равна = {A - B}");
+            Console.WriteLine($"Произведение чисел равно = {A * B}");
+            Console.WriteLine($"Частное чисел равно = {(double)A / (double)B}");
             Console.Read();
         }
     }
